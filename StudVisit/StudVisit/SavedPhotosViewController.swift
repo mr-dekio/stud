125ce7111528226a11378cb70e780fa7b42a169b
Maxim Galayko <maxim.galayko@kindgeek.com>
2016-05-27 21:34:23 +0300

    template for check in

--- StudVisit/StudVisit/SavedPhotosViewController.swift
+++ StudVisit/StudVisit/SavedPhotosViewController.swift
@@ -97,6 +97,44 @@ class SavedPhotosViewController: UIViewController {
     private func compareTimestampWithCurrent(date: NSDate) -> Bool {
         return true
     }
+    
+    
+    // MARK: - Calculate distance
+    
+    private func distanceBetweenCoordinates(first: CLLocationCoordinate2D, second: CLLocationCoordinate2D) -> Double {
+        let r: Double = 6371 // earth radius
+        let dLat = degreesToRadians(second.latitude - first.latitude)
+        let dLon = degreesToRadians(second.longitude - first.longitude)
+        let a = sin(dLat / 2.0) * sin(dLat / 2.0)
+            + cos(degreesToRadians(first.latitude)) * cos(degreesToRadians(second.latitude))
+            * sin(dLon / 2.0) * sin(dLon / 2.0)
+        let c = 2.0 * atan2(sqrt(a), sqrt(1 - a))
+        let distance = r * c
+        
+        return distance
+    }
+    
+    private func degreesToRadians(degrees: Double) -> Double {
+        return degrees * (M_PI / 180.0)
+    }
+    
+//    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+//    var R = 6371; // Radius of the earth in km
+//    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+//    var dLon = deg2rad(lon2-lon1);
+//    var a =
+//    Math.sin(dLat/2) * Math.sin(dLat/2) +
+//    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+//    Math.sin(dLon/2) * Math.sin(dLon/2)
+//    ;
+//    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+//    var d = R * c; // Distance in km
+//    return d;
+//    }
+//    
+//    function deg2rad(deg) {
+//    return deg * (Math.PI/180)
+//    }
 }
 
 extension SavedPhotosViewController: UITableViewDataSource, UITableViewDelegate {
